03d89ba060f3bcd30e1a19f8a8da38e25aae3507
Vương <quocvuong04122004@gmail.com>
2026-06-14 23:15:14 +0700

    doneMainUi

---
+++ Assets/Script/UI/SceneController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Nút PLAY
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Nút MENU
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
